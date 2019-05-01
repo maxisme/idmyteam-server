aa6e37ae55c60ef132b6d3e6d47317559997e459
Maximilian Mitchell <max@max.me.uk>
2019-05-01 14:29:20 +0100

    .

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,7 +1,7 @@
 import tornado
 from tornado.testing import AsyncHTTPTestCase
 
-from web import web_urls, web
+import web
 
 class TestWebApp(AsyncHTTPTestCase):
     url_blacklist = [
@@ -10,11 +10,11 @@ class TestWebApp(AsyncHTTPTestCase):
         '/upload'
     ]
     def get_app(self):
-        self.app = tornado.web.Application(web_urls.www_urls, **web.server_settings)
+        self.app = tornado.web.Application(web.web_urls.www_urls, **web.server_settings)
         return self.app
 
     def test_urls(self):
-        for url in web_urls.www_urls:
+        for url in web.web_urls.www_urls:
             if url[1].__module__ != 'events' and url[0] not in self.url_blacklist:
                 response = self.fetch(url[0])
                 assert response.code == 200
