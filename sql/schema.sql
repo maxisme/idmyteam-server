ea97f8fae649d1a6cf133a35951e656966fba2b6
Maximilian Mitchell <max@max.me.uk>
2019-05-07 15:54:00 +0100

    .

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,43 @@
 SET FOREIGN_KEY_CHECKS=0;
-DROP table IF EXISTS `Accounts`;
+CREATE TABLE `Logs` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(200) NOT NULL,
+  `method` varchar(200) NOT NULL,
+  `yaxis` varchar(200) NOT NULL DEFAULT '',
+  `name` varchar(200) NOT NULL,
+  `value` float NOT NULL,
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB 63 DEFAULT CHARSET=utf8;
+CREATE TABLE `Login` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `username` varchar(64) CHARACTER SET utf8 NOT NULL DEFAULT '',
+  `ip` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`),
+  CONSTRAINT `Login_ibfk_1` FOREIGN KEY (`username`) REFERENCES `Accounts` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `IP` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `pub` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `local` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+CREATE TABLE `Features` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `class` int(11) NOT NULL,
+  `features` varchar(2000) NOT NULL DEFAULT '',
+  `type` enum('MANUAL','MODEL') DEFAULT NULL,
+  `score` float NOT NULL DEFAULT '0',
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `create_dttm` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `new` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `account_username` (`username`),
+  CONSTRAINT `Features_ibfk_1` FOREIGN KEY (`username`) REFERENCES `Accounts` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB 1 DEFAULT CHARSET=utf8;
 CREATE TABLE `Accounts` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(64) NOT NULL,
@@ -20,51 +58,6 @@ CREATE TABLE `Accounts` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-DROP table IF EXISTS `Features`;
-CREATE TABLE `Features` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `class` int(11) NOT NULL,
-  `features` varchar(2000) NOT NULL DEFAULT '',
-  `type` enum('MANUAL','MODEL') DEFAULT NULL,
-  `score` float NOT NULL DEFAULT '0',
-  `username` varchar(64) NOT NULL DEFAULT '',
-  `create_dttm` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `new` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `account_username` (`username`),
-  CONSTRAINT `Features_ibfk_1` FOREIGN KEY (`username`) REFERENCES `Accounts` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=121 DEFAULT CHARSET=utf8;
-DROP table IF EXISTS `IP`;
-CREATE TABLE `IP` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `pub` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
-  `local` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-DROP table IF EXISTS `Login`;
-CREATE TABLE `Login` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `username` varchar(64) CHARACTER SET utf8 NOT NULL DEFAULT '',
-  `ip` varchar(100) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  KEY `username` (`username`),
-  CONSTRAINT `Login_ibfk_1` FOREIGN KEY (`username`) REFERENCES `Accounts` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-DROP table IF EXISTS `Logs`;
-CREATE TABLE `Logs` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `type` varchar(200) NOT NULL,
-  `method` varchar(200) NOT NULL,
-  `yaxis` varchar(200) NOT NULL DEFAULT '',
-  `name` varchar(200) NOT NULL,
-  `value` float NOT NULL,
-  `username` varchar(64) NOT NULL DEFAULT '',
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3863 DEFAULT CHARSET=utf8;
-SET FOREIGN_KEY_CHECKS=1;
-
-DROP view IF EXISTS `Account_Users`;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `Account_Users` AS select `Features`.`username` AS `username`,count(distinct `Features`.`class`) AS `num_classes` from `Features` group by `Features`.`username`;
+SET FOREIGN_KEY_CHECKS=1;
