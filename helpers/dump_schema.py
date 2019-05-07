1911390ac31b9343143396c30537e9a6b845a5a5
Maximilian Mitchell <max@max.me.uk>
2019-05-07 15:06:29 +0100

    .

--- helpers/dump_schema.py
+++ helpers/dump_schema.py
@@ -11,8 +11,8 @@ x.close()
 
 
 x = conn.cursor()
-schema = 'SET FOREIGN_KEY_CHECKS=0;'
-for result in results:
+schema = 'SET FOREIGN_KEY_CHECKS=0;\n'
+for result in results[::-1]:
     try:
         x.execute("show create table " + result[0])
         scheme = x.fetchall()[0][1]
@@ -26,5 +26,5 @@ for result in results:
 schema += 'SET FOREIGN_KEY_CHECKS=1;'
 
 schema = schema.replace('DEFINER=`{}`@`localhost` '.format(config.DB["db"]), '')
-schema = re.sub(r"AUTO_INCREMENT=\d. ", '', schema)  # remove AUTO_INCREMENT
+schema = re.sub(r"AUTO_INCREMENT=\d.", '', schema)  # remove AUTO_INCREMENT
 print(schema)
