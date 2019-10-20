50c2a956dcf1d362b18079833f8664e8ae99a357
Maximilian Mitchell <max@maxis.me>
2019-10-20 21:36:41 +0100

    remove log level in attempt to write logs

--- settings/logs.py
+++ settings/logs.py
@@ -6,8 +6,6 @@ logging.getLogger("tornado.access").disabled = True
 logger = logging.getLogger()
 c_handler = logging.StreamHandler()
 f_handler = logging.FileHandler(ROOT + '/idmyteam.log')
-c_handler.setLevel(logging.DEBUG)
-f_handler.setLevel(logging.DEBUG)
 c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 c_handler.setFormatter(c_format)
