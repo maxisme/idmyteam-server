1990d7c71a2248c412c7a4efdaa1bdd8c30ed4c8
Maximilian Mitchell <max@maxis.me>
2019-10-20 21:32:57 +0100

    logger not writing to file - attempt fix

--- settings/logs.py
+++ settings/logs.py
@@ -3,11 +3,11 @@ from settings.config import ROOT
 
 logging.getLogger("tornado.access").disabled = True
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 c_handler = logging.StreamHandler()
 f_handler = logging.FileHandler(ROOT + '/idmyteam.log')
-c_handler.setLevel(logging.INFO)
-f_handler.setLevel(logging.INFO)
+c_handler.setLevel(logging.DEBUG)
+f_handler.setLevel(logging.DEBUG)
 c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 c_handler.setFormatter(c_format)
