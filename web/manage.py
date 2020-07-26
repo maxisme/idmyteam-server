83e9474fe8c14b2b1a9151886645a65580f41831
Maximilian Mitchell <max@maxis.me>
2020-07-26 14:32:01 +0100

    move tracer stuff

--- web/manage.py
+++ web/manage.py
@@ -3,11 +3,36 @@
 import os
 import sys
 from opentelemetry.ext.django import DjangoInstrumentor
-
+from opentelemetry.ext import jaeger
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (BatchExportSpanProcessor, SimpleExportSpanProcessor, ConsoleSpanExporter)
+from web.settings import DEBUG
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
     os.environ.setdefault("OPENTELEMETRY_PYTHON_DJANGO_INSTRUMENT", True)
+
+    trace.set_tracer_provider(TracerProvider())
+    tracer = trace.get_tracer(__name__)
+
+    if DEBUG:
+        # print tracer
+        trace.get_tracer_provider().add_span_processor(
+            SimpleExportSpanProcessor(ConsoleSpanExporter())
+        )
+    else:
+        # jaeger tracer
+        jaeger_exporter = jaeger.JaegerSpanExporter(
+            service_name='ID My Team',
+            collector_host_name='jaeger-collector',
+            collector_port=14268
+        )
+        trace.get_tracer_provider().add_span_processor(BatchExportSpanProcessor(jaeger_exporter))
+
+    with tracer.start_as_current_span('started ID My Team'):
+        print('Started!')
+
     DjangoInstrumentor().instrument()
     try:
         from django.core.management import execute_from_command_line
--- web/web/urls.py
+++ web/web/urls.py
@@ -14,32 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from opentelemetry.ext import jaeger
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (BatchExportSpanProcessor, SimpleExportSpanProcessor, ConsoleSpanExporter)
-
-from web.settings import DEBUG
-
-trace.set_tracer_provider(TracerProvider())
-tracer = trace.get_tracer(__name__)
-
-if DEBUG:
-    # print tracer
-    trace.get_tracer_provider().add_span_processor(
-        SimpleExportSpanProcessor(ConsoleSpanExporter())
-    )
-else:
-    # jaeger tracer
-    jaeger_exporter = jaeger.JaegerSpanExporter(
-        service_name='ID My Team',
-        collector_host_name='jaeger-collector',
-        collector_port=14268
-    )
-    trace.get_tracer_provider().add_span_processor(BatchExportSpanProcessor(jaeger_exporter))
-
-with tracer.start_as_current_span('started ID My Team'):
-    print('Started!')
 
 urlpatterns = [
     path("", include("idmyteamserver.urls")),
