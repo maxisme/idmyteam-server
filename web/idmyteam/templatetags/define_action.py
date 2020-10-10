ae4cb6c077d575f4d3b4f4e2334160b875b51685
Maximilian Mitchell <max@max.me.uk>
2020-10-10 22:08:00 +0100

    fix svg slow loading

--- web/idmyteam/templatetags/define_action.py
+++ web/idmyteam/templatetags/define_action.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -6,3 +7,9 @@ register = template.Library()
 @register.simple_tag
 def define(val=None):
     return val
+
+
+@register.simple_tag
+def include_file(file_name):
+    file = open(file_name).read()
+    return mark_safe(file)
