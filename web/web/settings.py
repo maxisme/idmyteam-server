47368d25d0b615f61bc4568dc88ed300d095bd76
Maximilian Mitchell <max@max.me.uk>
2020-10-23 16:20:40 +0100

    remove unused requirements.txt

--- web/web/settings.py
+++ web/web/settings.py
@@ -102,12 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-PASSWORD_HASHERS = [
-    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
-    "django.contrib.auth.hashers.Argon2PasswordHasher",
-]
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.BCryptSHA256PasswordHasher"]
 
 # redis
 REDIS_HOST = os.environ.get("REDIS_HOST") or "127.0.0.1"
