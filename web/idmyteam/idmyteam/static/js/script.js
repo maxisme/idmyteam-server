059e68e0f416d0c86ff8db3109ce60dd6547d649
Maximilian Mitchell <max@max.me.uk>
2020-11-06 11:17:35 +0000

    add materialize js

--- web/idmyteam/idmyteam/static/js/script.js
+++ web/idmyteam/idmyteam/static/js/script.js
@@ -1,7 +1,17 @@
 $(function(){
-	$(".button-collapse").sideNav();
-
 	$('#alert-close').click(function(){
 		$( "#alert-box" ).fadeOut("slow");
 	});
+
+	/* materialize stuff */
+	$(".button-collapse").sideNav();
+	$('select').material_select();
+	$('.modal').modal();
+
+    function fadeInOut() {
+        var el = $(".flashme");
+        el.animate({opacity:'0.2'}, 1000);
+        el.animate({opacity:'1'}, 1000, fadeInOut);
+    }
+    fadeInOut();
 });
