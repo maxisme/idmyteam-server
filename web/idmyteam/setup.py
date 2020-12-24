8f212d244e08db33ae6fddf7cad4d6ea27f5015d
Max Mitchell <max.mitchell@ly.st>
2020-12-24 00:40:30 +0000

    try fix pypi upload

--- web/idmyteam/setup.py
+++ web/idmyteam/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+import setuptools
 import os
 
 version = os.getenv("VERSION")
@@ -6,5 +6,14 @@ if not version:
     print("missing VERSION env variable")
     quit(1)
 
-
-setup(include_package_data=True, version=version)
+setuptools.setup(
+    name="idmyteam",
+    description="Base django files for idmyteam-client and idmyteam-server",
+    version=version,
+    url="https://idmy.team/",
+    author="Maximilian Mitchell",
+    author_email="max@max.me.uk",
+    license="BSD-3-Clause",
+    include_package_data=True,
+    packages=setuptools.find_packages()
+)
